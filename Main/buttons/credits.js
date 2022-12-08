66194522ad123d4ba7f461891c0d693af80c54e6
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2022-12-08 09:55:43 -0800

    Update credits.js

--- Main/buttons/credits.js
+++ Main/buttons/credits.js
@@ -1,1 +1,26 @@
+function makeElem(varNm, it, st, sl, w) {
+         varNm.innerText = it;
+        
+         varNm.style.position = "absolute";
+        
+         varNm.style.top = `${st}`;
+         varNm.style.left = `${sl}`;
+        
+         varNm.style.zIndex = `2147483647`;
+         varNm.style.fontFamily = 'Nunito';
+         varNm.style.color = 'purple';
+        
+         varNm.style.borderRadius = '7.5px';
+         varNm.style.borderWidth = 'thick';
+         varNm.style.borderColor = 'red';
+        
+         varNm.style.backgroundColor = "orange";
+         varNm.style.width = w;
+         varNm.style.height = "30px";
+       
+        // styles SUUUUUIII
+         document.body.appendChild(varNm);
+         
+};
+
 const credits = document.createElement("button"); makeElem(credits, "Credits", "75px", "20px","99px"); credits.onclick = function() { try { alert("Made by Kahmron, this is the first version of the Rumi Panel!"); } catch { console.log("Failed to load.") } }
