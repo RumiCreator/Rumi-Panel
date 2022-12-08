5ed067c1b9d430835d5b3fc752d669edafdadb9b
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2022-12-08 13:01:43 -0800

    Update crashpage.js

--- Main/buttons/crashpage.js
+++ Main/buttons/crashpage.js
@@ -1,3 +1,28 @@
+ function makeElem(varNm, it, st, sl, w) {
+         varNm.innerText = it;
+        
+         varNm.style.position = "absolute";
+        
+         varNm.style.top = `${st}`;
+         varNm.style.left = `${sl}`;
+        
+         varNm.style.zIndex = `2147483647`;
+         varNm.style.fontFamily = 'Nunito';
+         varNm.style.color = 'purple';
+        
+         varNm.style.borderRadius = '7.5px';
+         varNm.style.borderWidth = 'thick';
+         varNm.style.borderColor = 'red';
+        
+         varNm.style.backgroundColor = "orange";
+         varNm.style.width = w;
+         varNm.style.height = "30px";
+       
+        // styles SUUUUUIII
+         document.body.appendChild(varNm);
+         
+      };
+
 // Crash page button, please use at your own risk.
       const CrashPage = document.createElement("button");
       makeElem(CrashPage, "Crash Page!", "100px", "20px", "99px");
