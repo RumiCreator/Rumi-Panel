1fce394ad0648aa49aa3a877547348799a58c551
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2022-12-08 15:35:44 -0800

    Update choosehackbutton.js

--- Main/buttons/choosehackbutton.js
+++ Main/buttons/choosehackbutton.js
@@ -1,34 +1,41 @@
 function makeElem(varNm, it, st, sl, w) {
-         varNm.innerText = it;
-        
-         varNm.style.position = "absolute";
-        
-         varNm.style.top = `${st}`;
-         varNm.style.left = `${sl}`;
-        
-         varNm.style.zIndex = `2147483647`;
-         varNm.style.fontFamily = 'Nunito';
-         varNm.style.color = 'purple';
-        
-         varNm.style.borderRadius = '7.5px';
-         varNm.style.borderWidth = 'thick';
-         varNm.style.borderColor = 'red';
-        
-         varNm.style.backgroundColor = "orange";
-         varNm.style.width = w;
-         varNm.style.height = "30px";
-       
-        // styles SUUUUUIII
-         document.body.appendChild(varNm);
-         
+    varNm.innerText = it;
+
+    varNm.style.position = "absolute";
+
+    varNm.style.top = `${st}`;
+    varNm.style.left = `${sl}`;
+
+    varNm.style.zIndex = `2147483647`;
+    varNm.style.fontFamily = 'Nunito';
+    varNm.style.color = 'purple';
+
+    varNm.style.borderRadius = '7.5px';
+    varNm.style.borderWidth = 'thick';
+    varNm.style.borderColor = 'red';
+
+    varNm.style.backgroundColor = "orange";
+    varNm.style.width = w;
+    varNm.style.height = "30px";
+
+    // styles SUUUUUIII
+    document.body.appendChild(varNm);
+
 };
 const chooseHackButton = document.createElement("button");
 makeElem(chooseHackButton, "Load Hack", "25px", "20px", "99px");
 
-chooseHackButton.onclick = function () {
-try {
-console.log("Empty command")
-} catch {
-console.error("what the hell is wrong with Javascript!?!?!?!?")
-}
+const Hacks = [
+    ["Rickroll", "off"]
+];
+
+chooseHackButton.onclick = function() {
+    try {
+        let pr1 = prompt("Hack?")
+        if (pr1.match(\Rickroll\gi)) {
+         location.href = "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwiwm8-7sI72AhVGO-wKHSBgDJUQwqsBegQIBhAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DxvFZjo5PgG0&usg=AOvVaw1bR_FN-SnfIGtKD3fjAA5P";
+        }
+    } catch {
+        console.error("what the hell is wrong with Javascript!?!?!?!?")
+    }
 }
