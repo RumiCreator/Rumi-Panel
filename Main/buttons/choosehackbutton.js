633180ea487441dd5e2e5f9cff14c6a3829c3bed
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2022-12-08 15:36:39 -0800

    Update choosehackbutton.js

--- Main/buttons/choosehackbutton.js
+++ Main/buttons/choosehackbutton.js
@@ -34,6 +34,7 @@ chooseHackButton.onclick = function() {
         let pr1 = prompt("Hack?")
         if (pr1.match(\Rickroll\gi)) {
          location.href = "https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&ved=2ahUKEwiwm8-7sI72AhVGO-wKHSBgDJUQwqsBegQIBhAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DxvFZjo5PgG0&usg=AOvVaw1bR_FN-SnfIGtKD3fjAA5P";
+         Hacks["Rickroll"] = "on";
         }
     } catch {
         console.error("what the hell is wrong with Javascript!?!?!?!?")
