2b2e75606e5dd9b3e6a1c56dbde8f5aa51696029
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2022-12-08 13:01:05 -0800

    Update crashpage.js

--- Main/buttons/crashpage.js
+++ Main/buttons/crashpage.js
@@ -1,1 +1,18 @@
+// Crash page button, please use at your own risk.
+      const CrashPage = document.createElement("button");
+      makeElem(CrashPage, "Crash Page!", "100px", "20px", "99px");
+      
 
+      CrashPage.onclick = function() {
+        try {
+         // hehe, just gotta y'know crash the page >:)
+          
+          
+          while (true) {
+            console.log("Crashed page, succesfully!");
+         }
+        } catch {
+          console.error("_Error_ RumiPanel couldn't proccess the hack."); 
+          alert("There was a problem executing this button");
+        } 
+      }
