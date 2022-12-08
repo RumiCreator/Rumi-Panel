974fb72a4c82d49fdbbb126fceab5fdcda1854b7
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2022-12-08 09:48:14 -0800

    Update checklvlbutton.js

--- Main/buttons/checklvlbutton.js
+++ Main/buttons/checklvlbutton.js
@@ -1,1 +1,8 @@
 
+
+const checklvlButton = document.createElement("button");
+      makeElem(checklvlButton, "Check Level", "50px", "20px", "99px");
+
+      checklvlButton.onclick = function() {
+         alert(`Your hack level is: {${Number(hackLevel)}}`);
+      };
