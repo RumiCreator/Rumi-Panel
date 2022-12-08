156b3871c14b004aa7a12873a4a20e276379cb63
RumiCreator <120061061+RumiCreator@users.noreply.github.com>
2022-12-08 09:48:49 -0800

    Update checklvlbutton.js

--- Main/buttons/checklvlbutton.js
+++ Main/buttons/checklvlbutton.js
@@ -1,4 +1,27 @@
-
+ function makeElem(varNm, it, st, sl, w) {
+         varNm.innerText = it;
+        
+         varNm.style.position = "absolute";
+        
+         varNm.style.top = `${st}`;
+         varNm.style.left = `${sl}`;
+        
+         varNm.style.zIndex = `2147483647`;
+         varNm.style.fontFamily = 'Nunito';
+         varNm.style.color = 'purple';
+        
+         varNm.style.borderRadius = '7.5px';
+         varNm.style.borderWidth = 'thick';
+         varNm.style.borderColor = 'red';
+        
+         varNm.style.backgroundColor = "orange";
+         varNm.style.width = w;
+         varNm.style.height = "30px";
+       
+        // styles SUUUUUIII
+         document.body.appendChild(varNm);
+         
+      };
 
 const checklvlButton = document.createElement("button");
       makeElem(checklvlButton, "Check Level", "50px", "20px", "99px");
